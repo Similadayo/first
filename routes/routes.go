5b5c5ef01fa74c8da0e01eb8f5b83115775e0303
similadayo <similoluwailesanmi@outlook.com>
2023-02-05 09:23:40 +0100

    controller Routes

---
+++ routes/routes.go
@@ -0,0 +1,51 @@
+package routes
+
+import (
+	"github.com/Similadayo/controllers"
+	"github.com/Similadayo/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func InitializeRoutes(r *gin.Engine) {
+	r.GET("/", controllers.HomeController)
+
+	//Register and login
+	userController := r.Group("/")
+	{
+		userController.POST("register", controllers.CreateUser)
+		userController.POST("login", controllers.Login)
+		userController.POST("logout", controllers.Logout)
+
+		userController.GET("getusers", middleware.Authorization, controllers.GetUsers)
+		userController.GET("getuser/:id", middleware.Authorization, controllers.GetUser)
+
+		userController.POST("updatepassword/:id", middleware.Authorization, controllers.ChangePassword)
+		userController.POST("forgotpassword", controllers.ForgotPassword)
+
+		userController.PUT("updateuser/:id", middleware.Authorization, controllers.UpdateUser)
+		userController.DELETE("deactivate/:id", middleware.Authorization, controllers.DeleteUser)
+		userController.PUT("suspend/:id", middleware.Authorization, controllers.SuspendUser)
+	}
+
+	//Category routes
+	categoryController := r.Group("/category")
+	{
+		categoryController.GET("/", middleware.Authorization, controllers.GetCategories)
+		categoryController.GET("/:id", middleware.Authorization, controllers.GetCategory)
+		categoryController.POST("/create", middleware.Authorization, controllers.CreateCategory)
+		categoryController.PUT("/update/:id", middleware.Authorization, controllers.UpdateCategory)
+		categoryController.DELETE("/delete/:id", middleware.Authorization, controllers.DeleteCategory)
+	}
+
+	// Product routes
+	productController := r.Group("/product")
+	{
+		productController.GET("/", middleware.Authorization, controllers.GetProducts)
+		productController.GET("/:id", middleware.Authorization, controllers.GetProduct)
+		productController.GET("/category/:id", middleware.Authorization, controllers.GetProductsByCategory)
+		productController.POST("/createproduct", middleware.Authorization, controllers.CreateProduct)
+		productController.PUT("/updateproduct/:id", middleware.Authorization, controllers.UpdateProduct)
+		productController.DELETE("/deleteproduct/:id", middleware.Authorization, controllers.DeleteProduct)
+	}
+
+}
